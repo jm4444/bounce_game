8bbe400baeaeefc937bad2bab0d90151ed3c718c
Justin Meredith <jm.4444@icloud.com>
2018-04-28 21:01:58 -0500

    The menu screen now displays the background. The code is very repetitive at this point, and once the game is complete I will work on isolating the classes so that they can be reused, as well as overall DRYing out the code. It's a work in progress.

--- menu_screen.py
+++ menu_screen.py
@@ -19,3 +19,50 @@ game.init()
 
 graphics_path = "graphics/"
 fps_clock = game.time.Clock()
+
+
+
+#   ~   ~   ~   ~   CLASSES   ~   ~   ~   ~   ~#
+
+class Board:
+    def __init__(self, image_file):
+        self.file_name = image_file
+        self.background = game.image.load(graphics_path + self.file_name)
+        self.size = width, height = get_image_size.get_image_size(graphics_path + self.file_name)
+        self.screen = game.display.set_mode(self.size)
+
+    def display(self):
+        self.screen.blit(self.background, (0, 0))
+
+
+
+#   ~   ~   ~   ~   FUNCTIONS   ~   ~   ~   ~   ~#
+
+def update_display():
+    board.display()
+
+
+
+#   ~   ~   ~   ~   SETTING THE MENU   ~   ~   ~   ~   ~#
+
+board = Board("menu.png")
+update_display()
+
+
+#   ~   ~   ~   ~   RUNNING THE GAME   ~   ~   ~   ~   ~#
+
+while True:
+
+    #//// Check for Specific Events //
+
+    for event in game.event.get():
+        if event.type == game.QUIT:      # allows the player to exit the game by clicking the exit 'X' on the window
+            game.quit()
+            raise SystemExit
+
+
+    #// Variables for Running the Game
+    fps_clock.tick(60)      # sets the frame rate at 60fps
+    game.event.pump()
+    update_display()
+    game.display.update()
