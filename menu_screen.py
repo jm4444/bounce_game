1cb6e1c276876e84b58f46747ea40a8e458a9915
Justin Meredith <jm.4444@icloud.com>
2018-04-28 20:42:42 -0500

    Adds the modules and set up section

---
+++ menu_screen.py
@@ -0,0 +1,21 @@
+"""
+
+_  __  ___b o u n c e___  __  _
+        a Pong knock off
+
+by Justin Meredith.
+project started March 16th, 2018.
+
+
+"""
+
+
+
+#   ~   ~   ~   ~   MODULES & SET UP   ~   ~   ~   ~   ~#
+
+import sys, pygame as game, get_image_size, random
+
+game.init()
+
+graphics_path = "graphics/"
+fps_clock = game.time.Clock()
