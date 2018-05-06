73e7bc385a4a11167b05862ced145643efca91f3
Justin Meredith <jm.4444@icloud.com>
2018-05-05 20:53:31 -0500

    Creates a new file for containing the functions. This was done because the functions are shared across files, so making them accessible by all files reduces repetition.

---
+++ functions.py
@@ -0,0 +1,43 @@
+"""
+
+_  __  ___b o u n c e___  __  _
+        a Pong knock off
+
+by Justin Meredith.
+project started March 16th, 2018.
+
+
+"""
+
+
+
+#   ~   ~   ~   ~   MODULES & SET UP   ~   ~   ~   ~   ~#
+
+import sys, pygame as game, get_image_size as image, random
+from functions import *
+
+game.init()
+
+graphics_path = "graphics/"
+
+
+
+#   ~   ~   ~   ~   FUNCTIONS   ~   ~   ~   ~   ~#
+
+def update_display(objects):
+    for object in objects:
+        object.display()
+
+def reset_positions(moving_objects, objects):
+    for object in moving_objects:
+        object.reset_position()
+    update_display(objects)
+    game.display.update()
+    hold_game()
+
+
+def hold_game():
+    game.time.delay(1500)
+
+def load_image(file_name):
+    return game.image.load(graphics_path + file_name)
